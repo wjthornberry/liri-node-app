e5b1d051ff78795bc7553473853f7082db72b59d
Jake Thornberry <wjthornberry@gmail.com>
2017-07-16 18:28:29 -0500

    Add key info to keys.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'sB1Dti2DOlJyjVWwoHTfPVY1u',
+  consumer_secret: '1mEgw1kh7Ig0nvvRDMcjAPgZZ9ffFQGTbtZlV1SIQd9UW2jawJ',
+  access_token_key: '886725576026882050-CrqnyplfAg9vtrQtyH1wLnfUewQwXag',
+  access_token_secret: 'LNZMjyapnWB0jlmUCvAkR0QG2My7llzn5kvuUxwKmpneP',
+}
