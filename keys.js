50396f17e7e84bf52d0fec64d90bb1f8f4207935
Jake Thornberry <wjthornberry@gmail.com>
2017-07-18 19:53:36 -0500

    Organize NPM packages

--- keys.js
+++ keys.js
@@ -5,4 +5,9 @@ exports.twitterKeys = {
   consumer_secret: '1mEgw1kh7Ig0nvvRDMcjAPgZZ9ffFQGTbtZlV1SIQd9UW2jawJ',
   access_token_key: '886725576026882050-CrqnyplfAg9vtrQtyH1wLnfUewQwXag',
   access_token_secret: 'LNZMjyapnWB0jlmUCvAkR0QG2My7llzn5kvuUxwKmpneP',
+};
+
+exports.spotifyKeys = {
+  consumer_key: 'c9d34ad9c1564802838cec8d2853b89a,
+  consumer_secret: '1dcd1a31221a492488eddb249833648b', 
 }
--- liri.js
+++ liri.js
@@ -1,17 +1,55 @@
-// NPM package for readinga and writing files
+// Import NPM packages
+
+// Readinga and writing files
 var fs = require('fs');
 
-// Allows the app to use the information from keys.js, 
-// where the Twitter API access keys are stored
-var keys = require("./keys.js");
-
-// NPM package to make http calls
+// Make http calls
 var request = require('request');
 
-// NPM package for the Twitter API
+// Twitter API
 var twitter = require('twitter');
 
-// NPM package for the Spotify API
+// Spotify API
 var spotify = require('spotify');
 
+// Pull  information from keys.js, where the Twitter API access keys are stored
+var keys = require("./keys.js");
+
+// Take in an input value
+var action = process.argv[2];
+
+
 // Open Movie Database (OMDb API)
+
+// Run the request function...
+// The request function takes in a URL then returns three arguments:
+// 1. It provides an error if one exists.
+// 2. It provides a response (usually that the request was successful)
+// 3. It provides the actual body text from the website.
+// request("http://www.omdbapi.com", function(error, response, body) {
+
+  // If the request was successful...
+//   if (!error && response.statusCode === 200) {
+
+    // Then log the body from the site!
+//     console.log(body);
+//   }
+// });
+
+// Bonus
+// 1) Output data to a .txt file
+// 2) Append each command ran to log.txt
+
+// This block of code will create a file called "log.txt".
+// It will then print "Inception, Die Hard" in the file
+// fs.writeFile("log.txt", "Inception, Die Hard", function(err) {
+
+  // If the code experiences any errors it will log the error to the console.
+//   if (err) {
+//     return console.log(err);
+//   }
+
+  // Otherwise, it will print: "log.txt was updated!"
+//   console.log("log.txt was updated!");
+
+// });
