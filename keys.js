3244d0f71b896792f789fe625570adbf2bc9ed67
Jake Thornberry <jakethornberry@Rocinante.local>
2017-07-23 21:53:22 -0500

    Reformat keys - declare as variables

--- keys.js
+++ keys.js
@@ -1,15 +1,20 @@
 console.log('this is loaded');
 
-// twitter keys
-exports.twitterKeys = ({
+// Twitter NPM 
+const Twitter = require('twitter');
+
+// Spotify NPM
+const Spotify = require('node-spotify-api');
+
+// new Twitter client for liri.js
+var client = new Twitter({
   consumer_key: 'sB1Dti2DOlJyjVWwoHTfPVY1u',
   consumer_secret: '1mEgw1kh7Ig0nvvRDMcjAPgZZ9ffFQGTbtZlV1SIQd9UW2jawJ',
   access_token_key: '886725576026882050-CrqnyplfAg9vtrQtyH1wLnfUewQwXag',
   access_token_secret: 'LNZMjyapnWB0jlmUCvAkR0QG2My7llzn5kvuUxwKmpneP',
 });
 
-// spotify keys
-exports.spotifyKeys = ({
+var spotify = new Spotify({
   consumer_key: 'c9d34ad9c1564802838cec8d2853b89a',
   consumer_secret: '1dcd1a31221a492488eddb249833648b', 
 });
